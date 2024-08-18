423c7c8314f0a306af108854ead97f929dc73daf
Mcampu <mcampuzano@smartbeemo.com>
2024-08-17 22:06:24 -0500

    fix text alignment, fix instagram link and add link to memory project

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text, Heading } from '@chakra-ui/react'
 import { FaInstagram, FaFacebook } from 'react-icons/fa'
 
 const HomePage = () => {
   return (
-  <Flex w={'100%'} h={'100vh'} gap={'1rem'} display={'flex'} flexDir={'column'} align={'center'} justify={'center'}>
+  <Flex w={'100%'} px={'20px'} h={'100vh'} gap={'1rem'} display={'flex'} flexDir={'column'} align={'center'} justify={'center'}>
     <h1 className="fs-1">Estamos soñando este sitio...</h1>
 
     <Flex w={'100%'} p={4} justify={'center'} flexDir={'column'} align={'center'}>
-      <p>Pero mientras tanto, puedes echarle un vistazo a nuestras redes.</p>
+      <Text textAlign={'center'}>Pero mientras tanto, puedes echarle un vistazo a nuestras redes.</Text>
         <Flex gap={'1rem'} p={4} color={'#c23b31'}>
-            <a href="https://www.instagram.com/casadelossuenos_sonson/?igsh=MW0xbjJkYjQ1amRoNQ%3D%3D" target="_blank" rel="noreferrer">
-              <FaInstagram size={32} color="#c23b31" />
+          <a href="https://www.instagram.com/casadelossuenossonson" target="_blank" rel="noreferrer">
+              <FaInstagram size={40} color="#c23b31" />
             </a>
             <a href="https://www.facebook.com/profile.php?id=61552995154276&mibextid=ZbWKwL" target="_blank" rel="noreferrer">
-              <FaFacebook size={32} color="#c23b31" />
+              <FaFacebook size={40} color="#c23b31" />
             </a>
         </Flex>
+        <Text textAlign={'center'}>O puedes echarle un vistazo a nuestro proyecto de memoria haciendo click
+          <a href="/memoria" target="_blank" rel="noreferrer"> aquí</a>
+        </Text>
     </Flex>
 
-    <h2 className="fs-4">Y recuerda, La calidad comienza en casa</h2>
+      <Heading as={'h3'} color={'#c23b31'} fontFamily={'Andor'} size="lg" textAlign="center">Y recuerda, La calidad comienza en casa</Heading>
   </Flex>
   )
 }
